39d92b8365539f9f7a988d289387f2702353d19b
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-07-12 20:37:38 +0530

    updated

--- Assignment4_TODOS/my-app/src/components/todoItem.jsx
+++ Assignment4_TODOS/my-app/src/components/todoItem.jsx
@@ -1,13 +1,9 @@
-export const TodoItem = ({todo,handlestatus,handleDelete})=>
-{
-
-    return (
-      <div>
-        {todo.id}-{todo.title} - {todo.status ? "Done":"Not Done"}
-      <button onClick={()=>handlestatus(todo.id)}>Toggle</button>
-      <button onClick={()=>handleDelete(todo.id)}>DELETE</button>
-      </div>
-    
-    )
-  
-}
+export const TodoItem = ({ todo, handlestatus, handleDelete }) => {
+  return (
+    <div>
+      {todo.id}-{todo.title} - {todo.status ? "Done" : "Not Done"}
+      <button onClick={() => handlestatus(todo.id)}>Toggle</button>
+      <button onClick={() => handleDelete(todo.id)}>DELETE</button>
+    </div>
+  );
+};
--- Assignment4_TODOS/my-app/src/components/todoinput.jsx
+++ Assignment4_TODOS/my-app/src/components/todoinput.jsx
@@ -1,18 +1,20 @@
 //import react from "react"
-import {useState} from "react"
+import { useState } from "react";
 
-
-export const Todoinput = ({getdata})=>{
-    const [text,setText] = useState("")
-    return <div>
-         <input onChange={(e)=>{
-             //console.log(e.target.value);
-             setText(e.target.value)
-         }} type="text" placeholder="Enter Your Todo" />
-         <button onClick={()=>getdata(text)}>Add1</button>
-         {/* {text} */}
+export const Todoinput = ({ getdata }) => {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          //console.log(e.target.value);
+          setText(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Your Todo"
+      />
+      <button onClick={() => getdata(text)}>Add1</button>
+      {/* {text} */}
     </div>
-     
-}
-
-
+  );
+};
